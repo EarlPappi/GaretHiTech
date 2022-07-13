cbef7fe95f5d4baa04f32c57a5177444bede356b
earlpappi <earlpappi@gmail.com>
2022-07-13 13:46:37 +0100

    icon section fixed

---
+++ src/Components/SliderSection/SliderSection.jsx
@@ -0,0 +1,30 @@
+import Audi from '../../Assets/audi.png';
+import Netflix from '../../Assets/netflix.png';
+import Maxar from '../../Assets/maxar.png';
+import SpaceX from '../../Assets/space-x.png';
+import classes from './SliderSection.module.css';
+import Wrapper from '../TinyComp/Wrapper';
+
+const SliderSection = () => {
+    return (
+        <section className={classes.sliderSection}>
+            <Wrapper>
+                <p>COMPANIES HIRING ON HERTECH</p>
+
+                <div className={ classes.iconCon }>
+                    <img src={Audi} alt="" />
+                    <img src={Netflix} alt="" />
+                    <img src={Audi} alt="" />
+                    <img src={SpaceX} alt="" />
+                    <img src={Audi} alt="" />
+                    <img src={Maxar} alt="" />
+                    <img src={Audi} alt="" />
+
+                </div>
+            </Wrapper>
+
+        </section>
+    );
+}
+
+export default SliderSection;
