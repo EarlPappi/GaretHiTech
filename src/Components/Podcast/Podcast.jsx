beb51f4964d4233c4d30ac54de65d32ba9550901
earlpappi <earlpappi@gmail.com>
2022-07-14 12:26:35 +0100

    podcast updates

--- src/Components/Podcast/Podcast.jsx
+++ src/Components/Podcast/Podcast.jsx
@@ -1,9 +1,25 @@
 import classes from "./Podcast.module.css"
 import Button from "../TinyComp/Button";
+import Thumbnail from "../../Assets/podcast-img.png"
 
 const Podcast = () => {
-    return ( 
-        <div className={ classes.podcastCon }>
+
+    const podcasts = [
+        {
+            title: "Women in Tech Series Q&A Series",
+            anchor: "Martyna Lewinska, Co-Founder and CTO at Fiat Republic",
+            duration: '12Mins',
+            thumbnail: Thumbnail
+        },
+        {
+            title: "Women in Tech Series Q&A Series",
+            anchor: "Martyna Lewinska, Co-Founder and CTO at Fiat Republic",
+            duration: '12Mins',
+            thumbnail: Thumbnail
+        }
+    ]
+    return (
+        <div className={classes.podcastCon}>
             <div className={classes.firstPodSec}>
                 <div>
                     <h2>Online Communities</h2>
@@ -14,8 +30,30 @@ const Podcast = () => {
                     <Button color="white" bgColor='#3754DB'>See all</Button>
                 </div>
             </div>
+
+            {podcasts.map((podcast) => {
+                return (
+                    <div className={classes.podcastListing}>
+                        <div className={classes.imgConPod}>
+                            <img src={podcast.thumbnail} alt={podcast.title} />
+                        </div>
+
+                        <div className={classes.podInner}>
+                            <h3>{podcast.title}</h3>
+                            <p>{podcast.anchor}</p>
+                            <div>
+                                <Button color='#3754DB' bgColor="#E2E8F0">{podcast.duration}</Button>
+                            </div>
+                        </div>
+
+
+                    </div>
+                )
+            })}
+
+
         </div>
-     );
+    );
 }
- 
+
 export default Podcast;
